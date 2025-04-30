6838e4242884d087ec5f90dc8eef0e05eac01972
Matheus Oliveira Silva <matheusolivsilv.ai@gmail.com>
2025-04-30 12:05:06 -0300

    feat: automated graph construction pipeline

--- apps/knowledge_graph/src/graph_builder.py
+++ apps/knowledge_graph/src/graph_builder.py
@@ -1,89 +1,92 @@
 import os
-import re
-import requests
-import urllib.parse
+import sys
+import json
+from pathlib import Path
+
+# Adicionar o diretório raiz ao Python path
+root_dir = str(Path(__file__).parent.parent.parent.parent)
+sys.path.append(root_dir)
+
 from dotenv import load_dotenv
+from apps.common.llm_config import LLMConfig
+from langchain_neo4j import Neo4jGraph
+from langchain_experimental.graph_transformers import LLMGraphTransformer
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.documents import Document
+from graph_structure_prompt import DB_GRAPH_PROMPT
 
 load_dotenv()
 
 class DBToGraph:
-    def __init__(self, schema):
+    def __init__(self, wow_tree_nodes_data_path: str) -> None:
+        self.wow_tree_nodes_data_path = wow_tree_nodes_data_path
+
         # Modify temperature and model_name experimentally if need some improvements.
-        self.llm = ChatOpenAI(temperature=0, model_name="gpt-4o")
+        self.llm = LLMConfig(provider="azure", model="gpt-4o").get_llm()
 
         # To use the neo4j graph, need to run neo4j.sh script. (for more info see README.md)
         self.graph = Neo4jGraph(refresh_schema=True)
 
         # Initialize the LLMGraphTransformer with the DB_GRAPH_PROMPT and create KB, after this send for Neo4j.
-        self.llm_transformer = self.initialize_db_graph_transformer()
-
-        # Augment the table infos with the LLM model and make langchain documents about it.
-        self.augmented_schema_chunks = self.transform_schema_to_langchain_documents(schema)
-
-    def initialize_db_graph_transformer(self):
-        return LLMGraphTransformer(
+        self.llm_transformer = LLMGraphTransformer(
             llm=self.llm,
             prompt=DB_GRAPH_PROMPT,
-            allowed_nodes=["TABLE", "COLUMN"],
-            allowed_relationships=[
-                "HAS_COLUMN",  # Links a table to its columns.
-                "REFERENCES"   # Links a source table to a referenced table based on foreign key relationships.
-            ],
-            node_properties=[
-                # Table properties
-                "table_name",  # Name of the table.
-                "columns",  # List of columns in the table.
-                "table_description",  # Description of the table's role.
-
-                # Column properties
-                "column_name",  # Name of the column.
-                "data_type",  # Data type of the column.
-                "column_description",  # Description of the column's role.
-            ],
-            relationship_properties=[
-                # REFERENCES relationship properties.
-                "relationship_description",  # Description of the relationship.
-                "foreign_key_column_name",  # Name of the column acting as a foreign key.
-                "referenced_table_name",  # Name of the table being referenced.
-                "referenced_column_name",  # Name of the column being referenced.
-                "referenced_column_description",  # Description of the referenced column's role.
-            ]
+            allowed_nodes=["BUFF", "SKILL"],
+            allowed_relationships=["PROCS", "CDR", "BUFFS"],
+            node_properties=["name", "description"],
+            relationship_properties=["description"],
         )
 
-    def augment_table_infos(self, table_infos):
-        chain = DATA_AUGMENTATION_PROMPT | self.llm | StrOutputParser()
-        return chain.invoke({"input_schema": table_infos})
+    def parse_json_items_to_langchain_documents(self, json_data):
+        # class_documents = []
+        # specs_documents = []
+        # hero_talents_documents = []
 
-    def transform_schema_to_langchain_documents(self, schema):
-        langchain_documents = []
-        augmented_infos_path = "schemas/augmented_schema.txt"
+        talents_documents = []
 
-        tables = schema.split("CREATE TABLE")
+        # Iterate over classes and specs
+        for class_name in json_data:
+            # For POC, only Shaman is used, uncomment for other classes when needed.    
+            if class_name == "Shaman":
+                for json_spec_data in json_data[class_name]['specs']:
+                    for node in json_spec_data['spec_nodes']:
+                        skill = node[0] + f" - {json_spec_data["spec_name"]} specialization talent:"
+                        description = node[1]
+                        talents_documents.append(Document(page_content=skill + "\n" + description))
 
-        if not os.path.exists(augmented_infos_path):
-            print("No augmented infos found, creating new chunks...")
-            # write augmented infos to file.
-            with open(augmented_infos_path, "w") as file:
-                for table in tables:
-                    augmented_schema_chunk = self.augment_table_infos(f"CREATE TABLE {table}")
-                    file.write(augmented_schema_chunk)
-                    file.write("\n---new-table-chunk---\n")
-                    langchain_documents.append(Document(page_content=augmented_schema_chunk))
-        else:
-            with open(augmented_infos_path, "r") as file:
-                augmented_schema_chunk = file.read()
-                chunks = augmented_schema_chunk.split("\n---new-table-chunk---\n")
-                for chunk in chunks:
-                    langchain_documents.append(Document(page_content=chunk))
-        return langchain_documents
+                    for node in json_spec_data['hero_talent_nodes']:
+                        skill = node[0] + f" - {json_spec_data["spec_name"]} hero talent:"
+                        description = node[1]
+                        talents_documents.append(Document(page_content=skill + "\n" + description))
+            
+                for node in json_data[class_name]['class_nodes']:
+                    skill = node[0] + f" - {class_name} class talent:"
+                    description = node[1]
+                    talents_documents.append(Document(page_content=skill + "\n" + description))
+
+            #print(class_documents)
+            # print(specs_documents)
+            # print(hero_talents_documents)
+                        
+        return talents_documents
 
     def create_kg(self):
-        for chunk in self.augmented_schema_chunks:
-            print("Chunk:", chunk)
-            graph_from_docs = self.llm_transformer.convert_to_graph_documents([chunk])
-            self.graph.add_graph_documents(graph_from_docs, include_source=True)
+        with open(self.wow_tree_nodes_data_path, "r") as file:
+            wow_tree_nodes_data = json.load(file)
+
+        talents_documents = self.parse_json_items_to_langchain_documents(wow_tree_nodes_data)
+
+        #print(talents_documents[0].page_content)
+        for document in talents_documents:
+            print("Document: ", document.page_content)
+            graph_from_docs = self.llm_transformer.convert_to_graph_documents([document])
+            self.graph.add_graph_documents(graph_from_docs, include_source=False)
 
 if __name__ == "__main__":
-    db_to_graph = DBToGraph(MONDIAL_SCHEMA)
+    
+    db_to_graph = DBToGraph(
+        wow_tree_nodes_data_path=root_dir + "/apps/etl/data/wow_talents_data.json"
+    )
+
     db_to_graph.create_kg()
     print("Graph created successfully!")
